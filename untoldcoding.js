cba783f28c0687648c48e9972fc8606ff4efa306
SANDEEP GANTA <153908262+sandeep7395@users.noreply.github.com>
2023-12-28 14:43:56 +0530

    Add files via upload

---
+++ untoldcoding.js
@@ -0,0 +1,18 @@
+var i = 0;
+var txt1 =
+  "Hi ";
+var speed = 55;
+typeWriter();
+function typeWriter() {
+  if (i < txt1.length) {
+    if (txt1.charAt(i) == "<")
+      document.getElementById("text1").innerHTML += "</br>";
+    else if (txt1.charAt(i) == ">")
+      document.getElementById("text1").innerHTML = "";
+    else if (txt1.charAt(i) == "|") {
+      $(".bg_heart").css("background-image", "')");
+    } else document.getElementById("text1").innerHTML += txt1.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
